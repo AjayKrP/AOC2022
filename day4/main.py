603144117005e23ed1ac6a785944f36ae6ee1c55
AjayKrP <ajayiii8@gmail.com>
2022-12-04 22:54:17 +0530

    code formatting

--- day4/main.py
+++ day4/main.py
@@ -6,11 +6,11 @@ with open("input.txt") as file:
         x1, y1 = list(map(int, cords[0].split("-")))
         x2, y2 = list(map(int, cords[1].split("-")))
         if (x1 <= x2 and y2 <= y1) or \
-            (x2 <= x1 and y1 <= y2):
+                (x2 <= x1 and y1 <= y2):
             part1 += 1
 
         if (x2 <= y1 and y2 >= x1) or \
-            (x1 <= y2 and y1 >= x2):
+                (x1 <= y2 and y1 >= x2):
             part2 += 1
 
 
